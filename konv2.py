641869e3635e4c10bbb4fccb2ec534524144bd9a
unknown <volirvag132005@bk.ru>
2012-09-23 23:56:35 +0400

    bla

---
+++ konv2.py
@@ -0,0 +1,15 @@
+x=str(raw_input("Please enter mounth:"))
+mes = { 'January':1,
+'Febrary':2,
+'March':3,
+'April':4,
+'May':5,
+'June':6,
+'July':7,
+'August':8,
+'September':9,
+'October':10,
+'November':11,
+'December':12}
+
+print mes[x]
---
+++ znak.py
@@ -0,0 +1,121 @@
+x=int(raw_input("Please enter number 0-9:"))
+
+if x > 9 or x<0:
+
+print "Error: enter the correct number!"
+
+elif x == 0:
+
+print   "********"
+print   "*      *"
+print   "*      *"
+print   "*      *"
+print   "*      *"
+print   "*      *"
+print   "*      *"
+print   "********"
+
+elif x==1:
+
+print   "        *"
+print   "       *"
+print   "       *"
+print   "       *"
+print   "       *"
+print   "       *"
+print   "       *"
+print   "       *"
+elif x==2:
+
+print   " *******"
+print   "*     * "
+print   "     *  "
+print   "    *   "
+print   "   *    "
+print   "  *     "
+print   " *      "
+print   "********"
+elif x==3:
+
+print   "********"
+print   "       *"
+print   "       *"
+print   "   *****"
+print   "       *"
+print   "       *"
+print   "       *"
+print   "********"
+
+elif x==4:
+
+print   "*      *"
+print   "*      *"
+print   "********"
+print   "       *"
+print   "       *"
+print   "       *"
+print   "       *"
+
+elif x==5:
+print   "********"
+print   "*       "
+print   "*       "
+print   "********"
+print   "       *"
+print   "       *"
+print   "       *"
+print   "********"
+elif x==6:
+print   "********"
+print   "*       "
+print   "*       "
+print   "********"
+print   "*      *"
+print   "*      *"
+print   "*      *"
+print   "********"
+elif x==7:
+
+print   "********"
+print   "      * "
+print   "      * "
+ print   "     *  "
+print   "    *   "
+print   "   *    "
+print   "  *     "
+print   " *      "
+print   "*       "
+
+elif x==8:
+
+print   "********"
+print   "      * "
+ print   "     *  "
+print   "    *   "
+print   "   *    "
+print   "  *     "
+print   " *      "
+print   "*       "
+
+elif x==8:
+
+print   "********"
+print   "*      *"
+print   "*      *"
+print   "********"
+print   "*      *"
+print   "*      *"
+print   "*      *"
+print   "********"
+
+elif x==9:
+print   "********"
+print   "*      *"
+print   "*      *"
+print   "********"
+print   "       *"
+print   "       *"
+print   "       *"
+print   "********"
+
+
